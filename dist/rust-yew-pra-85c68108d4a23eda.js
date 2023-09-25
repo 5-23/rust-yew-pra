08e97a4e5f0b75fe5eb4f616859363917508565b
5-23 <yhanbyeol6@gmail.com>
2023-09-26 07:54:58 +0900

    feat: add some style

--- dist/rust-yew-pra-85c68108d4a23eda.js
+++ dist/rust-yew-pra-85c68108d4a23eda.js
@@ -601,11 +601,11 @@ function __wbg_get_imports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper3746 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper3747 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 307, __wbg_adapter_16);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3917 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper3918 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 323, __wbg_adapter_19);
         return addHeapObject(ret);
     };
@@ -648,7 +648,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('rust-yew-pra-5177abf5b29f1810_bg.wasm', import.meta.url);
+        input = new URL('rust-yew-pra-85c68108d4a23eda_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,7 @@
-use stylist::css;
+mod component;
+
+use component::video::*;
 use yew::prelude::*;
-use stylist::Style;
-
-
-#[derive(PartialEq, Clone, Properties)]
-struct Video {
-    id: usize,
-    title: String,
-}
-
-#[derive(PartialEq, Properties)]
-pub struct VideoListProps {
-    video: Video,
-}
-
-#[function_component(VideoHtml)]
-pub fn video(VideoListProps { video }: &VideoListProps) -> Html {
-    let text = use_state(||"click here!".to_string());
-
-    let v = video.title.clone();
-
-    let onclick = {
-        let text = text.clone();
-        
-        move |_| {
-            text.set(format!("{v}"));
-        }
-    };
-
-
-    html! {
-        <>
-            <div {onclick}>
-            <h1 class={if *text == "click here!".to_string(){ css!(r#"background-color: #ddd;"#) }else{css!()}}>{(*text).clone()}</h1>   <h2>{video.id.to_string()}</h2>  </div>
-        </>
-    }
-}
 
 #[function_component]
 fn App() -> Html {
@@ -47,11 +13,11 @@ fn App() -> Html {
     // let videos = ;
     html! {
         <>
+            <VideoHtml video={video.clone()}/>
             <VideoHtml video={video}/>
         </>
     }
-}
-
+} 
 fn main() {
     yew::Renderer::<App>::new().render();
 }
